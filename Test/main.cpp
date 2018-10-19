d0c370ac81ce52016b8c638eb8e66067d7c3b039
dean <1328788930@qq.com>
2018-10-19 13:07:41 +0800

    update

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <TL.hpp>
+#include <shell.hpp>
+
 using namespace std;
+
+class root:public sys{
+public:
+	root(){
+		init("root");
+		add();
+	}
+	void add(){
+		
+	}
+};
 
 int main(){
     cout << "Test" << endl;
+    
+    root *r = new root();
+    cout << r->binding->n->name << endl;
+    
     test();
     return 0;
 }
